d5676bce8993247eccb7d11e92902c6264dc067b
salvi <salvio.calvillo@enti.cat>
2019-12-18 19:47:25 +0100

    SENDEAMOS

--- MaquinaServer/main.cpp
+++ MaquinaServer/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iostream>
 #include <SFML/Network.hpp>
 #include <unistd.h>
 #include <wait.h>
@@ -8,24 +9,41 @@ int main()
 
     sf::TcpListener dispatcher;
 
+
     sf::Socket::Status status = dispatcher.listen(50000);
 
-    sf::TcpSocket incoming;
+    sf::TcpSocket client;
 
 
     if(status != sf::Socket::Done)
     {
-        std::cout << "A\n";
     }
 
 
-    if(dispatcher.accept(incoming) != sf::Socket::Done)
+    if(dispatcher.accept(client) != sf::Socket::Done)
     {
-        std::cout << "B\n";
+    }
+    else
+    {
+
+
+
+            sf::Packet pack;
+            pack << "Hola \n";
+            sf::Socket::Status sendStatus = client.send(pack);
+            if(sendStatus == sf::Socket::Disconnected)
+            {
+            }
+            if (sendStatus != sf::Socket::Done)
+            {
+                std::cout << "Envio de datos fallido" << std::endl;
+            }
+
 
     }
 
-    incoming.disconnect();
+    client.disconnect();
     dispatcher.close();
     return 0;
 }
+
