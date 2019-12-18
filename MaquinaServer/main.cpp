258d289cf7c3d8bc5b2ac29190f13945834f29fb
salvi <salvio.calvillo@enti.cat>
2019-12-18 20:10:11 +0100

    push temporal

--- MaquinaServer/main.cpp
+++ MaquinaServer/main.cpp
@@ -3,34 +3,55 @@
 #include <SFML/Network.hpp>
 #include <unistd.h>
 #include <wait.h>
+#include <vector>
+#include <thread>
+
+void getNewConnections();
+bool gameRunning = true;
+std::vector<sf::TcpSocket*> sockets;
+sf::TcpListener dispatcher;
 
 int main()
 {
-
-    sf::TcpListener dispatcher;
-
-
     sf::Socket::Status status = dispatcher.listen(50000);
+    std::thread t(&getNewConnections);
+    t.detach();
 
-    sf::TcpSocket client;
-
-
-    if(status != sf::Socket::Done)
+    while(gameRunning)
     {
+        std::cout << "Hey, inserta E para cerrar: \n";
+        char exitChar;
+        std::cin >> exitChar;
+        if(exitChar == 'E' || exitChar == 'e')
+        {
+            gameRunning = false;
+        }
     }
-
-
-    if(dispatcher.accept(client) != sf::Socket::Done)
-    {
+    std::vector<sf::TcpSocket*>::iterator it;
+    for (it = sockets.begin(); it != sockets.end(); ++it){
+        (*it)->disconnect();
     }
-    else
+    dispatcher.close();
+    return 0;
+}
+
+
+void getNewConnections()
+{
+    while(gameRunning)
     {
-
-
-
+        sf::TcpSocket* temp = new sf::TcpSocket();
+        if (dispatcher.accept(*temp) != sf::Socket::Done)
+        {
+            std::cout << "Conexión no aceptada" << std::endl;
+        }
+        else
+        {
+            std::cout << "Conexión establecida con : " << temp << std::endl;
+            sockets.push_back(temp);
             sf::Packet pack;
             pack << "Hola \n";
-            sf::Socket::Status sendStatus = client.send(pack);
+            sf::Socket::Status sendStatus = temp->send(pack);
             if(sendStatus == sf::Socket::Disconnected)
             {
             }
@@ -40,10 +61,7 @@ int main()
             }
 
 
+        }
     }
-
-    client.disconnect();
-    dispatcher.close();
-    return 0;
 }
 
