b928e54743de465442c460a103fa3ca9d1c5c9f2
Eloi <eloi.garcia@enti.cat>
2020-01-03 20:27:43 +0100

    Organizacion login/mapselec/gems
    no he conseguido que conecte con el servidor aun no se porque

--- MaquinaCliente/main.cpp
+++ MaquinaCliente/main.cpp
@@ -27,11 +27,8 @@ void ReceivePacket(sf::TcpSocket* client)
         }
     }
 }
-bool SendPacket(sf::TcpSocket* _socket)
+bool SendPacket(sf::TcpSocket* _socket, std::string msg)
 {
-    std::string msg;
-    std::cout << "Insert message to send:" << std::endl;;
-    std::getline(std::cin, msg);
     sf::Packet infoToSend;
     infoToSend << msg;
     sf::Socket::Status sendStatus = _socket->send(infoToSend);
@@ -49,8 +46,14 @@ bool SendPacket(sf::TcpSocket* _socket)
 }
 int main()
 {
+    std::string user;
+    std::string password;
     ///CLIENTE
-    std::cout << "Cliente iniciado" << std::endl;
+    std::cout << "Cliente iniciado\n" << "Porfavor, introduzca su usuario\n"<<  std::endl;
+    std::cin >> user;
+    std::cout << "Ahora introduzca su contraseña" << std::endl;
+    std::cin >> password;
+    std::string verify = "VERIFY_"+user+"_"+password;
     size_t received;
     sf::TcpSocket* socket = new sf::TcpSocket();
     sf::Socket::Status status = socket->connect("127.0.0.1", 50001, sf::seconds(15.f)); //establcemos conexion con el server
@@ -61,11 +64,42 @@ int main()
     }
     else
     {
+        int eleccion;
         std::thread listener(&ReceivePacket, socket);
         listener.detach();
-        while(SendPacket(socket))
-        {
-            //   SendPacket(socket);
+        SendPacket(socket, verify)
+
+
+
+        std::cout << "Logging in\n" << std::endl;
+        wait(10);
+        //si no es correcto ALOMEJOR tendriamos que hacer un while(SendMessage(socket, msg) y hasta que no sea true ir repitiendo el cin de password y user
+        ///std::cout << "Error en el inicio de sesión, porfavor, introduzca de nuevo su usuario y contraseña."
+        ///std::cin << user;
+        ///std::cin << password;
+        //si el verify es correcto
+        std::cout << "1. Selección de mapa\n" << "2. Gemas\n" << "3. Salir\n" << std:endl;
+        std::cin >> eleccion;
+        if(elección == 1){
+            int mapa;
+            std::cout << "1. Mapa 1\n" << "2. Mapa 2\n" << std::endl;
+            if(mapa == 1){
+                SendPacket(socket, "SELECTMAP_"+mapa);
+                //selección mapa 1
+                //funcion de inicio de partida?
+            }
+            else if(mapa == 2){
+                SendPacket(socket, "SELECTMAP_"+mapa);
+                //selección mapa 2
+                //funcion de inicio de partida?
+            }
+        }
+        else if (eleccion == 2){
+            SendPacket(socket, "GETGEMS");
+            //mostrar gemas
+        }
+        else if(eleccion = 3){
+            exit(0);
         }
     }
     std::cout << "Cliente Terminado" << std::endl;
